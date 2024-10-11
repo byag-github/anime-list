8589ff1e714fce119d1972f9ca1a7460e93799a8
Ucup <fariyusuf368@gmail.com>
2024-10-11 20:54:37 +0700

    change title

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,7 +3,7 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
 export const metadata = {
-    title: "Anime List clone",
+    title: "Anime List Clone",
     description: "Website Anime List Created by: byag",
 };
 
